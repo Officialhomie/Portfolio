a22f66a19c970ac2a9840df6ad54758e46c5b34a
OneTrueHomie <vicigwilo@gmail.com>
2025-12-03 12:03:08 +0100

    Update wagmi configuration

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -4,10 +4,19 @@ import { injected } from 'wagmi/connectors'
 
 export const config = createConfig({
   chains: [base],
-  connectors: [injected()],
+  connectors: [
+    injected({
+      target: 'metaMask',
+    }),
+    injected({
+      target: 'coinbaseWallet',
+    }),
+    injected(),
+  ],
   transports: {
     [base.id]: http(process.env.NEXT_PUBLIC_BASE_RPC_URL || 'https://mainnet.base.org'),
   },
+  ssr: false,
 })
 
 export default config
