e48716f563906a5c1cc64f6e087b786b8466f271
OneTrueHomie <vicigwilo@gmail.com>
2025-12-16 19:04:45 +0100

    refactor: remove old lib files for new modular structure
    - Remove old contracts.ts, errors.ts, ipfs.ts
    - Remove old projects-data.ts, projects-ipfs.ts, projects.ts
    - Remove old utils.ts, wagmi.ts
    - Prepare for new src/lib modular structure with better organization

--- lib/projects.ts
+++
@@ -1,6 +0,0 @@
-// Re-export from projects-data for backward compatibility
-// NOTE: Do NOT re-export IPFS functions here to avoid SSR issues
-// Import IPFS functions directly from './projects-ipfs' when needed
-export * from './projects-data'
-
-
