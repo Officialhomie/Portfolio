4ebb8e60697dd4c20e3a7bb4af1f6a41cabf9e15
OneTrueHomie <vicigwilo@gmail.com>
2025-12-22 14:53:26 +0100

    fix: export both PasskeyAccountFactory and BiometricAccountFactory alias
    - Export PasskeyAccountFactory directly for new code
    - Keep BiometricAccountFactory alias for backward compatibility
    - Allows both import styles to work

--- src/lib/cdp/factory/index.ts
+++ src/lib/cdp/factory/index.ts
@@ -3,5 +3,6 @@
  */
 
 export type { IAccountFactory } from './IAccountFactory';
+export { PasskeyAccountFactory } from './BiometricAccountFactory';
 export { PasskeyAccountFactory as BiometricAccountFactory } from './BiometricAccountFactory';
 
