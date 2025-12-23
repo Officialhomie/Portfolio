d7e19478396fb60ac6636515ba429102a81788ac
OneTrueHomie <vicigwilo@gmail.com>
2025-12-23 23:56:19 +0100

    refactor: update signers index exports

--- src/lib/cdp/signers/index.ts
+++ src/lib/cdp/signers/index.ts
@@ -4,6 +4,5 @@
 
 export type { ISigner, SignerOptions, SignerFactory } from './ISigner';
 export { isSigner } from './ISigner';
-export { WebAuthnSigner } from './WebAuthnSigner';
 export { EOASigner } from './EOASigner';
 
